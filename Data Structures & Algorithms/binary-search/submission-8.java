fee8275a5acd0ca99f23b42e4365d91eadcb2cdf
Zachary Carruthers <144139268+ZacharyCarruthers@users.noreply.github.com>
2026-07-09 02:09:51 -0400

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+            int index = nums.length / 2;
+            int leftBound = 0;
+            int rightBound = nums.length - 1;
+
+            do{
+
+                if(nums[index] == target){
+                    return index;
+                } else if (nums[index] < target){
+                    leftBound = index + 1;
+                } else if (nums[index] > target){
+                    rightBound = index - 1;
+                }
+
+                index = leftBound + ((rightBound - leftBound) / 2);
+
+            } while(leftBound <= rightBound);
+
+            return -1;
+
+
+    }
+}
